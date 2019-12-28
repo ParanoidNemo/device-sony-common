ad358f9f88332e1187ef629e901e460d8aae9eee
Andrea Calzavacca <paranoid.nemo@gmail.com>
2019-12-28 13:38:36 +0100

    remove ipacfg inclusion

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -37,7 +37,8 @@ include device/sony/common/hardware/qcom/utils.mk
 
 include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
-include $(call first-makefiles-under,$(ipa-hal))
+# 191228 VERIFY IF NEEDED, CREATE LOOP ERROR WHEN BUILDING
+#include $(call first-makefiles-under,$(ipa-hal))
 include $(call all-makefiles-under,$(gps-hal))
 include $(call all-makefiles-under,$(media-hal))
 endif
